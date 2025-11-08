8eac984de9f1df0df9981b3809320eb6fa220a8d
Tuchila Adi <adibogdan2004@gmail.com>
2025-11-08 20:16:23 +0200

    Proiect facut in 2023

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,95 @@
+using System.Collections.Specialized;
+using System.DirectoryServices.ActiveDirectory;
+using System.Runtime.CompilerServices;
+
+namespace Sorting_Algorithm_Visualizer
+{
+    public partial class Form1 : Form
+    {
+        int n;
+        int[] A;
+        PictureBox[,] P;
+        int c = 0;
+        int m = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+            //incarc sursa c++
+            StreamReader fin = new StreamReader("c++.txt");
+            string c = fin.ReadToEnd();
+
+            fin.Close();
+            ///incarc sursa c#
+            fin = new StreamReader("cs.txt");
+            c=fin.ReadToEnd();
+            */
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog4.InitialDirectory=Application.StartupPath;
+            openFileDialog4.Filter = "(*.in)|*.in";
+            openFileDialog4.FileName = "";
+            if(openFileDialog4.ShowDialog()==DialogResult.OK)
+            {
+                string f = openFileDialog4.FileName;
+                StreamReader fin = new StreamReader(f);
+                string l=fin.ReadLine();
+                string[] s = l.Split();
+                n = int.Parse(s[0]);
+                A=new int[n+1];
+                int maxim = 0;
+                for (int i = 1; i<= n; i++)
+                {
+                    A[i] = int.Parse(s[i]);
+                    if (A[i]>maxim)
+                        maxim= A[i];
+                }
+                m = maxim;
+                creare();
+                fin.Close();
+            }
+        }
+        private void creare()
+        {
+            panel1.Controls.Clear();
+            P=new PictureBox[n+1,m+1];
+            int L = panel1.Width;
+            int H = panel1.Height;
+            int w = Math.Min(L / m, H / n) - 3;
+            for (int i = 1; i<= n;i++)
+                for(int j=1;j<=m;j++)
+                {
+                    P[i, j] = new PictureBox();
+                    P[i,j].Width = w;
+                    P[i, j].Height = w;
+                    P[i, j].Top = (w + 2) * (i - 1);
+                    P[i, j].Left = (w + 2) * (j - 1);
+                    if (A[i] == 0) P[i, j].BackColor = Color.Aqua;
+                    else P[i, j].BackColor = Color.Maroon;
+                    panel1.Controls.Add(P[i, j]);
+                }
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+    }
+}
